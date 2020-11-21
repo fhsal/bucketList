e0a08d6a16ee13fe6defe21315a6748673c1b110
frank salvo <fhsalvo@yahoo.com>
2020-11-20 22:39:56 -0500

    updating server.js with default app.get

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.use(express.json());
 var routes = require("./controllers/bucketListController.js");
 app.use(routes);
 
+app.get('/', function(req, res){
+    res.redirect('/explore');
+ });
+
 app.listen(app.get('port'), function(){
     console.log('Server started on port '+app.get('port'))
 });
